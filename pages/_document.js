a51d640d99306447f9faabdc2e215312b488961b
awellsbiz <awellsbiz@gmail.com>
2024-01-02 15:33:24 -0800

    update

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" style={{scrollBehavior:'smooth'}}>
-      <Head />
+      <Head> 
+        <link rel="icon" href="public/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
